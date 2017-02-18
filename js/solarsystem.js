b3b6224d9aea79409020701b6d34d2a4742187cd
Ben Greaves <bsgreaves@gmail.com>
2017-02-18 11:13:41 -0600

    Done with solarsystem

--- js/solarsystem.js
+++ js/solarsystem.js
@@ -1,18 +1,9 @@
 var SolarSystem = (function(){
-	var planets = ["mercury", "uranus", "mars", "earth", "jupiter", "saturn", "neptune", "venus"];
-	var numPlanetsPeopleLandedOn = 0;
-	var stars = ["sun", "alpha centauri", "wolf 359"];
-	var age = 0;
+	var dwarfPlanets = ["pluto"];
+
 	return {
-		getPlanets: function() {
-			return planets;
-		},
-		getPlanetsPeopleLandedOn: function(){
-			return numPlanetsPeopleLandedOn;
-		},
-		setPlanetsPeopleLandedOn: function(){
-			numPlanetsPeopleLandedOn ++;
-			return console.log("New num is ", numPlanetsPeopleLandedOn);
+		getDwarfPlanets: function() {
+			return dwarfPlanets;
 		},
 	};
 })();
